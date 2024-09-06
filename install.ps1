20f580e5c760bd0bdf4122a93468730d12c339f3
Marcelo Aros <marcelo.aros@binarylamp.cl>
2024-09-05 20:15:03 -0400

    add dns configuration

--- install.ps1
+++ install.ps1
@@ -7,6 +7,7 @@ $folderDisk = ".\diskfolder"
 $customUserName = "canvas"
 $customuserPassword = "hola"
 $root_password = "hola"
+$customDomain = "canvas.local"
 
 if (Test-Path $rootfsFile) {
     Write-Host "The rootfs file exists, used existing."
@@ -51,17 +52,10 @@ if ($isExistFolderDisk -eq $false) {
     Copy-Item -Path ".\configs" -Destination "\\wsl$\$customDistroName\home\$customUserName\" -Recurse
 
     wsl -d $customDistroName /bin/bash -c "sudo chmod +x /home/canvas/install-canvas.sh && sudo /home/$customUserName/install-canvas.sh $customUserName $customuserPassword"
-}
 
-$wslIP = wsl -d $customDistroName hostname -I
 
-wsl -d $customDistroName
-
-function FunctionName {
-    param (
-        [string]$customDistroName,
-        [string]$customDomain
-    )
+    #############     DNS Configuration     #############
+    $wslIP = wsl -d $customDistroName hostname -I
 
     $hostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
 
@@ -71,4 +65,54 @@ function FunctionName {
         Write-Host "No se pudo obtener la IP de la distribución WSL." -ForegroundColor Red
         exit 1
     }
+    Write-Host "La IP de la distribución WSL $customDistroName es: $wslIP"
+
+    # Check if the hosts file exists
+    if (-not (Test-Path $hostsFilePath)) {
+        Write-Host "El archivo hosts no se encontró o no se tiene permisos de acceso." -ForegroundColor Red
+        exit 1
+    }
+
+    $hostsContent = Get-Content $hostsFilePath
+
+    # Check if there is any line that contains the customDomain
+    $domainLineIndex = $hostsContent | Select-String -Pattern "\s+$customDomain" | ForEach-Object { $_.LineNumber - 1 }
+
+    if ($domainLineIndex -ne $null) {
+        # If a line exists with the domain, it replaces it with the correct IP
+        Write-Host "The domain $customDomain already exists, updating the IP..."
+        $hostsContent[$domainLineIndex] = "$wslIP $customDomain"
+    } else {
+        # If it does not exist, add a new line to the end of the file
+        Write-Host "The domain $customDomain does not exist, adding new entry..."
+        $hostsContent += "$wslIP $customDomain"
+    }
+
+    # Write the updated content to the hosts file 
+    Set-Content -Path $hostsFilePath -Value $hostsContent -Force
+
+    Write-Host "Domain $customDomain added or updated in the hosts file." -ForegroundColor Green
+
+    # Confirmar el cambio
+    Write-Host "Current hosts file content:"
+    Get-Content $hostsFilePath | Out-String
+
+}
+
+
+
+
+
+
+
+
+
+
+function FunctionName {
+    param (
+        [string]$customDistroName,
+        [string]$customDomain
+    )
+
+    
 }
