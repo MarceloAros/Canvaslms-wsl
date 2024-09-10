bdd0b133c985c8afea83bb1d62e7fa335bf490a3
Marcelo Aros <marcelo.aros@binarylamp.cl>
2024-09-10 18:23:37 -0300

    removed duplicate code

--- install.ps1
+++ install.ps1
@@ -53,48 +53,5 @@ if ($isExistFolderDisk -eq $false) {
 
     wsl -d $customDistroName /bin/bash -c "sudo chmod +x /home/canvas/install-canvas.sh && sudo /home/$customUserName/install-canvas.sh $customUserName $customuserPassword"
 
-
-    #############     DNS Configuration     #############
-    $wslIP = wsl -d $customDistroName hostname -I
-
-    $hostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
-
-    # Get the IP of the WSL distribution
-    $wslIP = wsl -d $customDistroName hostname -I
-    if (-not $wslIP) {
-        Write-Host "No se pudo obtener la IP de la distribución WSL." -ForegroundColor Red
-        exit 1
-    }
-    Write-Host "La IP de la distribución WSL $customDistroName es: $wslIP"
-
-    # Check if the hosts file exists
-    if (-not (Test-Path $hostsFilePath)) {
-        Write-Host "El archivo hosts no se encontró o no se tiene permisos de acceso." -ForegroundColor Red
-        exit 1
-    }
-
-    $hostsContent = Get-Content $hostsFilePath
-
-    # Check if there is any line that contains the customDomain
-    $domainLineIndex = $hostsContent | Select-String -Pattern "\s+$customDomain" | ForEach-Object { $_.LineNumber - 1 }
-
-    if ($domainLineIndex -ne $null) {
-        # If a line exists with the domain, it replaces it with the correct IP
-        Write-Host "The domain $customDomain already exists, updating the IP..."
-        $hostsContent[$domainLineIndex] = "$wslIP $customDomain"
-    } else {
-        # If it does not exist, add a new line to the end of the file
-        Write-Host "The domain $customDomain does not exist, adding new entry..."
-        $hostsContent += "$wslIP $customDomain"
-    }
-
-    # Write the updated content to the hosts file 
-    Set-Content -Path $hostsFilePath -Value $hostsContent -Force
-
-    Write-Host "Domain $customDomain added or updated in the hosts file." -ForegroundColor Green
-
-    # Confirmar el cambio
-    Write-Host "Current hosts file content:"
-    Get-Content $hostsFilePath | Out-String
-
+    .\run.ps1
 }
