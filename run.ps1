ea7c89f7e4702f8ce0815beb8083269ecfa2d199
Marcelo Aros <marcelo.aros@binarylamp.cl>
2024-09-10 18:20:32 -0300

    fix overwrite entire hosts file

--- run.ps1
+++ run.ps1
@@ -22,20 +22,30 @@ if (-not (Test-IsElevated)) {
 Write-Host "Administrator privileges successfully granted." -ForegroundColor Green
 
 Set-Location $scriptDirectory
-$config = Get-Content -Raw -Path ".\config.json" | ConvertFrom-Json
-Start-Sleep -Seconds 5
 
+$config = Get-Content -Raw -Path ".\config.json" | ConvertFrom-Json
 
 $windowsIpv4 = (Get-NetIPAddress -InterfaceAlias "*WSL*" -AddressFamily IPv4).IPAddress
 $wslIpv4 = (wsl -d $config.distribution.custom_name hostname -I)
 
-# set canvas_lms custom domain
-#.\utils\ChangeHostsLine.ps1 -FILE_PATH "\\wsl$\$($config.distribution.custom_name)\etc\hosts" -IP "127.0.0.1" -DOMAIN "$($config.canvas_lms.custom_domain)"
+# canvas_lms set custom domain
+## WSL
+Copy-Item -Path "\\wsl$\$($config.distribution.custom_name)\etc\hosts" -Destination ".\hosts" -Force
+$wslPath = ((Resolve-Path ".\hosts") -replace '\\', '/' -replace '^C:', '/mnt/c')
+.\utils\ChangeHostsLine.ps1 -FILE_PATH .\hosts -IP "127.0.0.1" -DOMAIN "$($config.canvas_lms.custom_domain)"
+## Windows
 .\utils\ChangeHostsLine.ps1 -FILE_PATH "C:\Windows\System32\drivers\etc\hosts" -IP "$wslIpv4" -DOMAIN "$($config.canvas_lms.custom_domain)"
 
-# set lti_tool custom domain
-#.\utils\ChangeHostsLine.ps1 -FILE_PATH "\\wsl$\$($config.distribution.custom_name)\etc\hosts" -IP "$windowsIpv4" -DOMAIN "$($config.lti_tool.custom_domain)"
+Start-Sleep -Seconds 2
+
+# lti_tool set custom domain
+.\utils\ChangeHostsLine.ps1 -FILE_PATH .\hosts -IP "$windowsIpv4" -DOMAIN "$($config.lti_tool.custom_domain)"
+Start-Sleep -Seconds 1
+wsl -d $config.distribution.custom_name /bin/bash -c "sudo cp $wslPath /etc/hosts"
+## Windows
 .\utils\ChangeHostsLine.ps1 -FILE_PATH "C:\Windows\System32\drivers\etc\hosts" -IP "127.0.0.1" -DOMAIN "$($config.lti_tool.custom_domain)"
+
+Remove-Item -Path ".\hosts" -Force
 
 # start canvas related services
 wsl -d $config.distribution.custom_name /bin/bash -c "sudo mkdir -p /var/run/passenger-instreg && sudo chown -R www-data:www-data /var/run/passenger-instreg && sudo service postgresql start && sudo service redis-server start && sudo service apache2 start && sudo service canvas_init start"
