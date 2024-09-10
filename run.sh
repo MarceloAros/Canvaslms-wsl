f3d5a5af041887a1635cda1b2652847d1fb8f69d
Marcelo Aros <marcelo.aros@binarylamp.cl>
2024-09-10 15:11:36 -0300

    remove unused file

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /var/run/passenger-instreg \
-  && sudo chown -R www-data:www-data /var/run/passenger-instreg \
-  && sudo service postgresql start \
-  && sudo service redis-server start \
-  && sudo service apache2 start \
-  && sudo service canvas_init start
